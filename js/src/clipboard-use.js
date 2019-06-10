902bf0c61fe457452b5cc7f23cbe7ba46793cf02
VictorHuang <hsq_encourage@163.com>
2019-06-10 21:56:44 +0800

    Site updated: 2019-06-10 21:56:25

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,18 +1,3 @@
-  /*页面载入完成后，创建复制按钮*/
-  !function (e, t, a) { 
-    /* code */
-    var initCopyCode = function(){
-      var copyHtml = '';
-      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-      //fa fa-globe可以去字体库替换自己想要的图标
-copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
-      copyHtml += '</button>';
-      $(".highlight .code pre").before(copyHtml);
-      new ClipboardJS('.btn-copy', {
-          target: function(trigger) {
-              return trigger.nextElementSibling;
-          }
-      });
-    }
-    initCopyCode();
-  }(window, document);
+// build time:Mon Jun 10 2019 21:55:40 GMT+0800 (中国标准时间)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
+//rebuild by neat 
