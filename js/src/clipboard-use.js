7fe655fe15cbae2b7fd2d3a29c3a79ea60cfe5f8
VictorHuang <hsq_encourage@163.com>
2019-06-22 21:39:28 +0800

    Site updated: 2019-06-22 21:38:58

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Mon Jun 10 2019 21:55:40 GMT+0800 (中国标准时间)
+// build time:Sat Jun 22 2019 21:38:43 GMT+0800 (中国标准时间)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
