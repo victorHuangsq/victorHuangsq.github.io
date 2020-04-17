e26e9c8be1a0d6ac93f5b4488ac1452ceeb41dbd
VictorHuang <hsq_encourage@163.com>
2020-04-17 10:17:53 +0800

    Site updated: 2020-04-17 10:17:35

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 27 2020 18:34:11 GMT+0800 (中国标准时间)
+// build time:Fri Apr 17 2020 10:17:16 GMT+0800 (中国标准时间)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 27 2020 18:34:12 GMT+0800 (中国标准时间)
+// build time:Fri Apr 17 2020 10:17:16 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
