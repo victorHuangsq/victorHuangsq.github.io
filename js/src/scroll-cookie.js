560590bf12ec6ae3db50b7f429f2d0c7f3b368db
VictorHuang <hsq_encourage@163.com>
2025-02-10 22:02:47 +0800

    Site updated: 2025-02-10 22:02:08

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Feb 09 2025 13:58:16 GMT+0800 (中国标准时间)
+// build time:Mon Feb 10 2025 21:48:03 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
